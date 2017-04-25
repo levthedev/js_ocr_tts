75bcae4c5d744902f6110065cb40df75e78cb929
Lev Kravinsky <kravinskylev@gmail.com>
2017-04-25 16:21:08 -0400

    experimenting with canvas

--- client.js
+++ client.js
@@ -1,23 +1,44 @@
-var msg = new SpeechSynthesisUtterance();
-var voices = window.speechSynthesis.getVoices();
-msg.voice = voices[65];
-msg.voiceURI = 'native';
-msg.volume = 1;
-msg.rate = 1;
-msg.pitch = 1;
-msg.lang = 'en-US';
+document.addEventListener("DOMContentLoaded", function() {
+  msg = new SpeechSynthesisUtterance();
+  voices = window.speechSynthesis.getVoices();
+  msg.voice = voices[65];
+  msg.voiceURI = 'native';
+  msg.volume = 1;
+  msg.rate = 1;
+  msg.pitch = 1;
+  msg.lang = 'en-US';
+});
 
 function processFile(file) {
+  ocr(file);
+}
+
+// function drawImage(file) {
+//   var reader = new FileReader();
+//   reader.onload = function(e) {
+//      var img = new Image();
+//      img.onload = function() { context.drawImage(img, 0, 0) };
+//      img.src = e.target.result;
+//   };
+//   reader.readAsDataURL(file);
+// }
+
+function ocr(file) {
+  var result = document.getElementById('result');
   Tesseract.recognize(file).progress(function(message) {
     if (message.status == "recognizing text") {
-      document.getElementById('result').innerHTML = `Progress: ${Math.round(message.progress * 100)}%`;
+      result.innerHTML = `Progress: ${Math.round(message.progress * 100)}%`;
     } else {
-      document.getElementById('result').innerHTML = `Status: ${message.status}`;
+      result.innerHTML = `${message.status}: ${message.progress}`;
     }
   }).then(function(result) {
     var sanitizedText = result.text.split(/[^a-zA-Z0-9\s.,!$;:()-]/).join("");
-    document.getElementById('result').innerHTML = sanitizedText;
-    speak(sanitizedText);
+    if (sanitizedText) {
+      document.getElementById('result').innerHTML = sanitizedText;
+      speak(sanitizedText);
+    } else {
+      speak('Sorry, I could not find any text to read.')
+    }
   })
 }
 
