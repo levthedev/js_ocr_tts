1f302cd1473319d63c2cc03da8c1536e45d02cc7
Lev Kravinsky <kravinskylev@gmail.com>
2017-04-25 16:27:59 -0400

    clean up code, remove canvas

--- client.js
+++ client.js
@@ -1,48 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-  msg = new SpeechSynthesisUtterance();
-  voices = window.speechSynthesis.getVoices();
-  msg.voice = voices[65];
-  msg.voiceURI = 'native';
-  msg.volume = 1;
-  msg.rate = 1;
-  msg.pitch = 1;
-  msg.lang = 'en-US';
-});
+var msg = new SpeechSynthesisUtterance();
+var voices = window.speechSynthesis.getVoices();
+msg.voice = voices[65];
+
+function el(id) { return document.getElementById(id) }
 
 function processFile(file) {
-  ocr(file);
-}
-
-// function drawImage(file) {
-//   var reader = new FileReader();
-//   reader.onload = function(e) {
-//      var img = new Image();
-//      img.onload = function() { context.drawImage(img, 0, 0) };
-//      img.src = e.target.result;
-//   };
-//   reader.readAsDataURL(file);
-// }
-
-function ocr(file) {
-  var result = document.getElementById('result');
   Tesseract.recognize(file).progress(function(message) {
-    if (message.status == "recognizing text") {
-      result.innerHTML = `Progress: ${Math.round(message.progress * 100)}%`;
-    } else {
-      result.innerHTML = `${message.status}: ${message.progress}`;
-    }
+    log(message)
   }).then(function(result) {
     var sanitizedText = result.text.split(/[^a-zA-Z0-9\s.,!$;:()-]/).join("");
-    if (sanitizedText) {
-      document.getElementById('result').innerHTML = sanitizedText;
-      speak(sanitizedText);
-    } else {
-      speak('Sorry, I could not find any text to read.')
-    }
+    sanitizedText ? readAndUpdate(sanitizedText) : speak('Sorry, I could not find any text to read.');
   })
 }
 
 function speak(text) {
   msg.text = text;
   speechSynthesis.speak(msg);
+}
+
+function log(message) {
+  var result = el('result');
+  result.innerHTML = `${message.status}: ${Math.round(message.progress * 100)}%`;
+}
+
+function readAndUpdate(text) {
+  el('result').innerHTML = text;
+  speak(text);
 }
