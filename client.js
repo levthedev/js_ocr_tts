c4b11b85f325e37cdd9de301fe99c2d80f865499
Lev Kravinsky <kravinskylev@gmail.com>
2017-04-24 22:29:43 -0400

    styles

--- client.js
+++ client.js
@@ -1,26 +1,27 @@
-var msg = new SpeechSynthesisUtterance()
-var voices = window.speechSynthesis.getVoices()
-msg.voice = voices[65]
-msg.voiceURI = 'native'
-msg.volume = 1
-msg.rate = 1
-msg.pitch = 1
-msg.lang = 'en-US'
+var msg = new SpeechSynthesisUtterance();
+var voices = window.speechSynthesis.getVoices();
+msg.voice = voices[65];
+msg.voiceURI = 'native';
+msg.volume = 1;
+msg.rate = 1;
+msg.pitch = 1;
+msg.lang = 'en-US';
 
 function processFile(file) {
-  Tesseract.recognize(file).progress(message => {
-    if (message.status == "recognizing text") message = message.progress
-    console.log('progress: ', message)
-    document.getElementById('result').innerHTML = message
-  }).then(result => {
-    console.log(result)
-    var sanitizedText = result.text.split(/[^a-zA-Z0-9\s.,!$;:()-]/).join("")
-    document.getElementById('result').innerHTML = sanitizedText
-    speak(sanitizedText)
+  Tesseract.recognize(file).progress(function(message) {
+    if (message.status == "recognizing text") {
+      document.getElementById('result').innerHTML = `Progress: ${Math.round(message.progress * 100)}%`;
+    } else {
+      document.getElementById('result').innerHTML = `Status: ${message.status}`;
+    }
+  }).then(function(result) {
+    var sanitizedText = result.text.split(/[^a-zA-Z0-9\s.,!$;:()-]/).join("");
+    document.getElementById('result').innerHTML = sanitizedText;
+    speak(sanitizedText);
   })
 }
 
 function speak(text) {
-  msg.text = text
+  msg.text = text;
   speechSynthesis.speak(msg);
 }
---
+++ readme.md
@@ -0,0 +1,20 @@
+Demo - Chrome only, slow speed, poor quality results
+[X] Basic JS OCR
+[X] Basic JS TTS
+[X] Take pics with phone camera
+[ ] Progressive Web App
+
+MVP - Native app, medium speed, good results
+[ ] Native cross platform app
+[ ] Arduino camera clip
+[ ] Bluetooth connectivity to Arduino camera
+[ ] One tap picture taking
+[ ] Picture preprocessing
+[ ] Native TTS
+[ ] Read picture line by line
+[ ] Spellcheck
+
+Fully featured - Extra features, medium/fast speed, good/great results
+[ ] Count people in picture
+[ ] Save and recognize faces
+[ ] Translate text
